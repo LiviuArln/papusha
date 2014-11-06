26cbab4f5d389c3101ea411428d02ecc00f91f47
Liviu Rau <liviu_aurelian@yahoo.com>
2014-11-06 13:22:28 +0200

    X

--- modules/puppetdev/files/papply.bat
+++ modules/puppetdev/files/papply.bat
@@ -1,1 +1,1 @@
-runas /noprofile /user:Administrator puppet apply %USERPROFILE%/papusha/manifests/site.pp --modulepath=%USERPROFILE%/papusha/modules $*
+puppet apply %USERPROFILE%/papusha/manifests/site.pp --modulepath=%USERPROFILE%/papusha/modules $*
